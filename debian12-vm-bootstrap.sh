c9494a41578af14602c2c9d2da47aaa10866d875
tmrcmug <155239368+tmrcmug@users.noreply.github.com>
2024-08-17 12:23:36 -0500

    added initial set of common packages to install

--- debian12-vm-bootstrap.sh
+++ debian12-vm-bootstrap.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-# snippet 
+# snippet typing
 
+# wget https://github.com/rcmug/snippet-sandbox/blob/main/debian12-vm-bootstrap.sh
+
+# sudo apt install git -y
+# git clone https://github.com/rcmug/snippet-sandbox.git
+# bash snippet-sandbox/debian12-vm-bootstrap.sh
+
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get -y autoremove
+
+sudo apt-get install sudo -y
+
+sudo apt-get install emacs-nox -y
+sudo apt-get install build-essential -y
+sudo apt-get install net-tools -y
+sudo apt-get install tcpdump -y
+sudo apt-get install rsync -y
